80d986f874abd62f72cf1e158e7029d2ff4e57ad
anamoe <anam45188@gmail.com>
2022-08-10 12:51:54 +0700

    perbaikan

---
+++ kirimdata.php
@@ -0,0 +1,21 @@
+<?php 
+
+	$konek = mysqli_connect("127.0.0.1", "bwid1177_pemetaanjalanrusak", "pemetaanjalanrusak", "bwid1177_pemetaanjalanrusak");
+
+	$start_latitude = $_GET['start_latitude'];
+	$start_longitude = $_GET['start_longitude'];
+	$end_latitude = $_GET['end_latitude'];
+	$end_longitude = $_GET['end_longitude'];
+	$level_jalan = $_GET['level_jalan'];
+	$kecepatan = $_GET['kecepatan'];
+
+		mysqli_query($konek, "ALTER TABLE data_jalans AUTO_INCREMENT=1");
+
+	$simpan = mysqli_query($konek, "insert into data_jalans(start_latitude,start_longitude,end_latitude,end_longitude,level_jalan,kecepatan)values('$start_latitude','$start_longitude','$end_latitude','$end_longitude','$level_jalan','$kecepatan')");
+
+	if($simpan)
+		echo "Berhasil";
+	else
+		echo "Gagal";
+
+?>
